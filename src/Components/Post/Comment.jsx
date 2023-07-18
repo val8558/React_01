b812bdc0d4a24c0da1005441bbf1ca5f6702b9a4
val8558 <valmmir@hotmail.com>
2023-07-17 21:15:59 -0300

    criação do componente de comentário

---
+++ src/Components/Post/Comment.jsx
@@ -0,0 +1,19 @@
+import style from '.Comment.module.css'
+
+function Comment () {
+    return (
+        <>
+            <div className={style.comment}>
+                <img src="" alt="" />
+
+                <div className={style.commentBox}>
+                    <div className={style.commentContent}>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Comment 
